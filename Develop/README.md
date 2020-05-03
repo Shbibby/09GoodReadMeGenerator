9e173e2e1cb10b246335104d4e139d441b79f6be
Shbibby <joshstillion@gmail.com>
2020-05-02 23:55:05 -0400

    finalized classes - all works except readme formatting

---
+++ Develop/README.md
@@ -0,0 +1,39 @@
+
+    # 
+
+    
+    __________________________________________
+    # Table of Contents :
+    
+    1. [Installation](#Installation)
+    2. [Usage](#Usage)
+    3. [License](#License)
+    4. [Contributing](#Contributing)
+    5. [Tests](#Tests) 
+    __________________________________________
+
+    ## Installation <a name="Installation"></a>
+
+    Use the https GitHub download
+
+    ## Usage 
+
+       foobar.pluralize('word') # returns 'words'   foobar.pluralize('goose') # returns 'geese'   foobar.singularize('phenomena') # returns 'phenomenon'
+
+    ## License
+
+    [MIT](https://choosealicense.com/licenses/mit/)
+
+    ## Contributing
+
+    Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+    ## Tests
+
+    node test tester.test.js
+    __________________________________________
+    
+    Author email : fakeemail@email.com
+
+    ![Picture](https://avatars2.githubusercontent.com/u/60363855?v=4)
+    
--- Develop/index.js
+++ Develop/index.js
@@ -1,41 +1,6 @@
-const inquirer = require('inquirer')
+const inquirer = require('inquirer');
 const axios = require('axios');
-
-
-
-const testArr = {
-  "login": "Shbibby",
-  "id": 60363855,
-  "node_id": "MDQ6VXNlcjYwMzYzODU1",
-  "avatar_url": "https://avatars2.githubusercontent.com/u/60363855?v=4",
-  "gravatar_id": "",
-  "url": "https://api.github.com/users/Shbibby",
-  "html_url": "https://github.com/Shbibby",
-  "followers_url": "https://api.github.com/users/Shbibby/followers",
-  "following_url": "https://api.github.com/users/Shbibby/following{/other_user}",
-  "gists_url": "https://api.github.com/users/Shbibby/gists{/gist_id}",
-  "starred_url": "https://api.github.com/users/Shbibby/starred{/owner}{/repo}",
-  "subscriptions_url": "https://api.github.com/users/Shbibby/subscriptions",
-  "organizations_url": "https://api.github.com/users/Shbibby/orgs",
-  "repos_url": "https://api.github.com/users/Shbibby/repos",
-  "events_url": "https://api.github.com/users/Shbibby/events{/privacy}",
-  "received_events_url": "https://api.github.com/users/Shbibby/received_events",
-  "type": "User",
-  "site_admin": false,
-  "name": "Joshua Stillion",
-  "company": null,
-  "blog": "https://shbibby.github.io/08updatedPortfolio/",
-  "location": "Cumming Ga",
-  "email": null,
-  "hireable": null,
-  "bio": "Currently a student in a full stack development bootcamp for Georgia Tech. I love problem solving, design, and just making stuff in general. ",
-  "public_repos": 14,
-  "public_gists": 0,
-  "followers": 9,
-  "following": 8,
-  "created_at": "2020-01-27T19:33:00Z",
-  "updated_at": "2020-05-02T02:29:43Z"
-}
+const fs = require('fs');
 
 // Github API Caller
 class ApiCall {
@@ -61,10 +26,10 @@ class ApiCall {
         }
       })
       .then((response) => {
-        console.log("Data: ", response)
+        // console.log("Data: ", response)
       })
       .catch((error) => {
-        console.log("Error: ", error)
+        // console.log("Error: ", error)
       });
   }
 }
@@ -96,25 +61,24 @@ const readMeTemplate = {
 class GenerateReadMeFile {
   constructor(readMeData) {
     this.readMeData = readMeData;
-    this.formattedReadMe = "";
   }
   
-  async getFormattedReadMe() {
-    this.formattedReadMe = `
+  getFormattedReadMe() {
+    return `
     # ${this.readMeData.title}
 
     ${this.readMeData.description}
     __________________________________________
     # Table of Contents :
     
-    1. [Installation] (#Installation)
-    2. [Usage] (#Usage)
-    3. [License] (#License)
-    4. [Contributing] (#Contributing)
-    5. [Tests] (#Tests) 
+    1. [Installation](#Installation)
+    2. [Usage](#Usage)
+    3. [License](#License)
+    4. [Contributing](#Contributing)
+    5. [Tests](#Tests) 
     __________________________________________
 
-    ## Installation
+    ## Installation <a name="Installation"></a>
 
     ${this.readMeData.installation}
 
@@ -139,8 +103,6 @@ class GenerateReadMeFile {
 
     ![Picture](${this.readMeData.gitHubProfilePicture})
     `;
-
-    console.log(formattedReadMe);
   }
 }
 
@@ -188,20 +150,18 @@ class GetUserAnswers {
 }
 // Prompts user to answer questions
 
-
-
 async function makeMyFile() {
-  // const getAnswer = new GetUserAnswers();
-  // getAnswer.askQuestions();
-  // readMeTemplate.title = getAnswer.title
-  // readMeTemplate.description = getAnswer.description
+  const getAnswer = new GetUserAnswers();
+  await getAnswer.askQuestions();
+  readMeTemplate.title = getAnswer.title
+  readMeTemplate.description = getAnswer.description
 
-  // const apithing = new ApiCall();
-  // apithing.callApi("https://api.github.com/users/Shbibby");
+  const apithing = new ApiCall();
+  await apithing.callApi("https://api.github.com/users/Shbibby");
 
   const makeReadMe = new GenerateReadMeFile(readMeTemplate);
-  makeReadMe.GenerateReadMeFile();
-
+  var x = makeReadMe.getFormattedReadMe();
+  fs.writeFile("./README.md", x, function(err) {if (err) throw err;});
 } 
 
 makeMyFile();
